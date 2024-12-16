3f98543bcce10806ec32348715a6c20a3b244cc1
FedorVeli4ko <151904225+FedorVeli4ko@users.noreply.github.com>
2024-12-16 14:11:16 +0300

    find-max.py:changed

--- find-max.py
+++ find-max.py
@@ -1,4 +1,22 @@
 def find_max(num):
     return max(num)
-nums = [1, 2, 3, 4, 5]
+    if not num:
+        print ("Список не должен быть пустым")
+    maxn = num[0]
+    for n in num:
+        if n > maxn:
+            maxn = n
+    else:
+         return maxn
+    return max
+
+n = int(input("Введите количество чисел: "))
+if n <= 0:
+        print ("Количество чисел должно быть положительным")
+
+nums = []
+for i in range(n):
+        number = float(input(f"Введите число {i + 1}: "))
+        nums.append(number)
+
 print("Максимум:", find_max(nums))
